a7aaac94caed8e2a64d205acb6da89f8c8d206fd
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-08 19:26:34 +0800

    header updated

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -4,8 +4,8 @@ import "./header.scss";
 
 function Header() {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
+        <Navbar expand="lg">
+            <Container fluid>
                 <Navbar.Brand href="#home">
                     <img alt="Fileblizz Logo" src="" width="30" height="30" className="d-inline-block align-top"/>
                     {' '}
@@ -13,13 +13,14 @@ function Header() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '200px' }} navbarScroll>
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <Nav.Link href="#account">Account</Nav.Link>
-                        <Nav.Link href="#about">About us</Nav.Link>
+                    <Nav className="mx-auto my-2 my-lg-0" style={{ maxHeight: '200px' }} navbarScroll>
+                        <Nav.Link href="/home">Home</Nav.Link>
+                        <Nav.Link href="/pricing">Pricing</Nav.Link>
+                        <Nav.Link href="/account">Account</Nav.Link>
+                        <Nav.Link href="/about">About us</Nav.Link>
                     </Nav>
-                    <Button href="/signin">SIGN IN</Button>
+                    <Button href="/signin" variant="outline-primary" className="me-3">Log in</Button>
+                    <Button href="/signup">Sign up</Button>
                 </Navbar.Collapse>
             </Container>
 
