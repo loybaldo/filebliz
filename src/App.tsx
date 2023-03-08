14331c80f8a451205f7314767ea19bc2d85cf92f
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-09 06:57:51 +0800

    internal-server-error-page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./pages/404";
+import InternalServerErrorPage from "./pages/500";
 import HomePage from "./pages/home";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <HomePage/> }/>
+        <Route path="/internal-server-error" element={ <InternalServerErrorPage/> }/>
         <Route path="*" element={ <NotFoundPage/> }/>
       </Routes>
     </BrowserRouter>
--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Header from "../../components/common/header";
 import Footer from "../../components/common/footer";
-import Title404 from "../../components/title";
 import "./404.scss";
+import { Button, Container } from "react-bootstrap";
 
 
 function NotFoundPage() {
     return (
         <React.Fragment>
             <Header/>
-                <Title404/>
+            <Container className="p-5 mb-5 text-center">
+                <img width="100%" src={require("../../assets/illustrations/404_error_cute_cat.png")}
+                    alt="404 illustration"
+                    style={{maxWidth: 350, minWidth: 250}}/>
+                <h1>Not Found</h1>
+                <p>The page you were looking for does not exist.</p>
+                <Button href="/" style={{borderRadius: 15}} className="mt-2">Go Back to Home</Button>
+            </Container>
             <Footer/>
         </React.Fragment>
     );
