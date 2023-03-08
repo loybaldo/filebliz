0cc0532bd2716b506b73b0ecef171da2a132448d
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-08 21:55:47 +0800

    not-found-page updated

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -1,5 +1,4 @@
 import { Button, Container } from "react-bootstrap";
-import { BrowserRouter, Link } from "react-router-dom";
 import "./title.scss";
 
 // havent tested line 12
@@ -7,10 +6,13 @@ import "./title.scss";
 
 function Header() {
     return (
-        <Container>
-            <h1>404 Not Found</h1>
+        <Container className="p-5 mb-5 text-center">
+            <img width="100%" src={require("../../assets/illustrations/404_error_cute_cat.png")}
+                alt="404 illustration"
+                style={{maxWidth: 350, minWidth: 250}}/>
+            <h1>Not Found</h1>
             <p>The page you were looking for does not exist.</p>
-            <Button href="/">Go Back to Home</Button>
+            <Button href="/" style={{borderRadius: 15}} className="mt-2">Go Back to Home</Button>
         </Container>
     );
 }
