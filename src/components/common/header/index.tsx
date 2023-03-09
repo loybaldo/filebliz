872ea85017c113edec4bf65f0c068766afc539da
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-09 10:31:25 +0800

    header updated

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -4,7 +4,7 @@ import "./header.scss";
 
 function Header() {
     return (
-        <Navbar expand="lg">
+        <Navbar bg="white" expand="lg" className="shadow" sticky="top" style={{zIndex: 9}}>
             <Container fluid>
                 <Navbar.Brand href="#home">
                     <img alt="Fileblizz Logo" src="" width="30" height="30" className="d-inline-block align-top"/>
@@ -12,7 +12,7 @@ function Header() {
                     Fileblizz
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" style={{borderRadius: 15}}/>
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" className="p-2">
                     <Nav className="mx-auto my-2 my-lg-0" style={{ maxHeight: '200px' }} navbarScroll>
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/pricing">Pricing</Nav.Link>
