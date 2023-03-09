d15b6569d83c793a60d4a1f9d68516a43a47c4ca
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-09 20:06:17 +0800

    services updated

--- src/components/services/index.tsx
+++ src/components/services/index.tsx
@@ -15,40 +15,40 @@ function Services() {
                         
                         <Col className="mb-5">
                             <Card className="shadow-sm" style={{borderRadius: 20}}>
+                                <Card.Img variant="top" src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpgholder.js/800x400?text=First slide&bg=373940" />
                                 <Card.Body className="card-body px-4 py-5 px-md-5">
                                     <h5 className="fw-bold card-title">Lorem ipsum dolor sit&nbsp;</h5>
-                                    <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
-                                    <Button className="shadow" type="button" style={{borderRadius: 15}}>Learn more</Button>
+                                    <p className="text-muted card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
                                 </Card.Body>
                             </Card>
                         </Col>
 
                         <Col className="mb-5">
                             <Card className="shadow-sm" style={{borderRadius: 20}}>
+                            <Card.Img variant="top" src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpgholder.js/800x400?text=First slide&bg=373940" />
                                 <Card.Body className="card-body px-4 py-5 px-md-5">
                                     <h5 className="fw-bold card-title">Lorem ipsum dolor sit&nbsp;</h5>
-                                    <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
-                                    <Button className="shadow" type="button" style={{borderRadius: 15}}>Learn more</Button>
+                                    <p className="text-muted card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
                                 </Card.Body>
                             </Card>
                         </Col>
 
                         <Col className="mb-5">
                             <Card className="shadow-sm" style={{borderRadius: 20}}>
+                            <Card.Img variant="top" src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpgholder.js/800x400?text=First slide&bg=373940" />
                                 <Card.Body className="card-body px-4 py-5 px-md-5">
                                     <h5 className="fw-bold card-title">Lorem ipsum dolor sit&nbsp;</h5>
-                                    <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
-                                    <Button className="shadow" type="button" style={{borderRadius: 15}}>Learn more</Button>
+                                    <p className="text-muted card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
                                 </Card.Body>
                             </Card>
                         </Col>
 
                         <Col className="mb-5">
                             <Card className="shadow-sm" style={{borderRadius: 20}}>
+                            <Card.Img variant="top" src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpgholder.js/800x400?text=First slide&bg=373940" />
                                 <Card.Body className="card-body px-4 py-5 px-md-5">
                                     <h5 className="fw-bold card-title">Lorem ipsum dolor sit&nbsp;</h5>
-                                    <p className="text-muted card-text mb-4">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
-                                    <Button className="shadow" type="button" style={{borderRadius: 15}}>Learn more</Button>
+                                    <p className="text-muted card-text">Erat netus est hendrerit, nullam et quis ad cras porttitor iaculis. Bibendum vulputate cras aenean.</p>
                                 </Card.Body>
                             </Card>
                         </Col>
