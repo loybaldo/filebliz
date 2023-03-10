2c9d60c75f6dc59f2d12ef06a9345984ff85207b
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-10 08:45:29 +0800

    login page added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFoundPage from "./pages/404";
 import InternalServerErrorPage from "./pages/500";
 import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
 import UploadPage from "./pages/upload";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <HomePage/> }/>
+        <Route path="/signin" element={ <LoginPage/> }/>
         <Route path="/upload" element={ <UploadPage/> }/>
         <Route path="/internal-server-error" element={ <InternalServerErrorPage/> }/>
         <Route path="*" element={ <NotFoundPage/> }/>
