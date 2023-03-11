27a8252bb96769783f3cb64a44cc712fb6755a25
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-11 11:04:42 +0800

    env supported

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import env from "react-dotenv";
 import "./home.scss";
 import Header from "../../components/common/header";
 import Footer from "../../components/common/footer";
 import Services from "../../components/services";
 import Slider from "../../components/slider";
-import Upload from "../../components/upload";
 
 
 function HomePage() {
