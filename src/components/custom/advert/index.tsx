0cab93121c8a04c7a531cff9f451bc8408dbdbe9
XPRO <79130038+loybaldo@users.noreply.github.com>
2023-03-15 05:44:14 +0000

    advert updated

--- src/components/custom/advert/index.tsx
+++ src/components/custom/advert/index.tsx
@@ -1,5 +1,4 @@
 import Button from "../../common/button";
-import Loader from "../../common/loader";
 import "./advert.scss";
 
 
@@ -8,6 +7,7 @@ function Advert() {
         <div className="f-advert">
             <img src="" alt="Advertisement" />
             <div>
+                <i className="fa-regular fa-crown" style={{fontSize: 24}}></i>
                 <span>Upgrade to Premium</span>
                 <p>Unlock the ability to upload larger files and enjoy advanced sharing features. Upgrade now for an enhanced file sharing experience.</p>
                 <Button label="Upgrade"/>
