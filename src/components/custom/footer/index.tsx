386d1581352f23754163e3005fd95b8311e933c4
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-16 12:51:31 +0800

    links secured

--- src/components/custom/footer/index.tsx
+++ src/components/custom/footer/index.tsx
@@ -7,13 +7,13 @@ function Footer() {
         <footer className="f-footer">
             <div className="f-container">
                 <div>
-                    <a href="https://facebook.com" target="_blank">
+                    <a href="https://facebook.com" target="_blank" rel="noreferrer">
                         <i className="fa-brands fa-facebook" style={{fontSize: 24}}></i>
                     </a>
-                    <a href="https://facebook.com" target="_blank">
+                    <a href="https://instagram.com" target="_blank" rel="noreferrer">
                         <i className="fa-brands fa-square-instagram" style={{fontSize: 24}}></i>
                     </a>
-                    <a href="https://facebook.com" target="_blank">
+                    <a href="https://twitter.com" target="_blank" rel="noreferrer">
                         <i className="fa-brands fa-twitter" style={{fontSize: 24}}></i>
                     </a>
                 </div>
