76064f0ec84f67585402a6473f14795e7ac3fce6
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-17 14:45:34 +0800

    offers updated

--- src/components/custom/offers/index.tsx
+++ src/components/custom/offers/index.tsx
@@ -5,7 +5,7 @@ import "./offers.scss";
 function Offers() {
     return (
         <div className="f-offers">
-            <PricingCard type="free" price={0} shortDesc="For Getting Started" features={["Lorem ipsum", "Dolor sit amet"]}/>
+            <PricingCard type="free" price={0} shortDesc="For Getting Started" features={["Lorem ipsum", "Dolor sit amet"]} action={false}/>
             <PricingCard type="free" price={0} shortDesc="For Getting Started" features={["Lorem ipsum", "Dolor sit amet"]}/>
             <PricingCard type="free" price={0} shortDesc="For Getting Started" features={["Lorem ipsum", "Dolor sit amet"]}/>
         </div>
