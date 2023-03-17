e3b056d128354fb612fe81e7b0fc4dbf1752ffa9
SlamTheDragon <derfeljohn2002@gmail.com>
2023-03-17 18:21:29 +0800

    Initial account pull

--- src/components/custom/navigation/index.tsx
+++ src/components/custom/navigation/index.tsx
@@ -19,7 +19,7 @@ function Navigation() {
                 <nav className="f-links">
                     <Link className={(location.pathname === "/") ? "f-links-item f-links-active" : "f-links-item"} to="/">Home</Link>
                     <Link className={(location.pathname === "/premium") ? "f-links-item f-links-active" : "f-links-item"} to="/premium">Premium</Link>
-                    <Link className={(location.pathname === "/account") ? "f-links-item f-links-active" : "f-links-item"} to="/account">Account</Link>
+                    <Link className={(location.pathname === "/no-account") ? "f-links-item f-links-active" : "f-links-item"} to="/no-account">Account</Link>
                     <Link className={(location.pathname === "/about") ? "f-links-item f-links-active" : "f-links-item"} to="/about">About</Link>
                 </nav>
                 <Button href="/signin" label="Sign in" style={{animation:"slidedown5 0.5s ease-in-out"}}/>
@@ -28,7 +28,7 @@ function Navigation() {
             <nav className="f-btm-nav">
                 <Link className={(location.pathname === "/") ? "f-btm-nav-item f-btm-nav-active" : "f-btm-nav-item"} to="/"><Icon icon={(location.pathname === "/") ? Icons.home : Icons.home_outline_bold}/></Link>
                 <Link className={(location.pathname === "/premium") ? "f-btm-nav-item f-btm-nav-active" : "f-btm-nav-item"} to="/premium"><Icon icon={(location.pathname === "/premium") ? Icons.store : Icons.store_outline_bold}/></Link>
-                <Link className={(location.pathname === "/account") ? "f-btm-nav-item f-btm-nav-active" : "f-btm-nav-item"}  to="/account"><Icon icon={(location.pathname === "/account") ? Icons.user : Icons.user_outline_bold}/></Link>
+                <Link className={(location.pathname === "/no-account") ? "f-btm-nav-item f-btm-nav-active" : "f-btm-nav-item"}  to="/no-account"><Icon icon={(location.pathname === "/no-account") ? Icons.user : Icons.user_outline_bold}/></Link>
                 <Link className={(location.pathname === "/about") ? "f-btm-nav-item f-btm-nav-active" : "f-btm-nav-item"}  to="/about"><Icon icon={(location.pathname === "/about") ? Icons.question_circle : Icons.question_circle_outline_bold}/></Link>
             </nav>
         </>
--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,14 +1,74 @@
 import Footer from "../../components/custom/footer";
 import Navigation from "../../components/custom/navigation";
+import "./account.scss"
+import Image from '../../assets/logo-full192.png'
+import Divider from "../../components/custom/divider";
 
+interface AccountInterface {
+    accountName: string
+    memberInfo: string
+    expiryDay: number
+    expiryRange: string
+    itemCount: number
+}
 
-function Account() {
+function Account({ accountName, memberInfo, expiryDay, expiryRange, itemCount } : AccountInterface) {
     return (
         <>
             <Navigation />
             <div style={{ marginTop: 60 }}></div>
 
-            <h1>Empty</h1>
+            <div className="f-account-container">
+            
+                <div className="f-account-overview">
+
+
+                    <div className="f-account-header">
+                        <div className="f-account-photo">
+                            {/* jQuery loader? */}
+                            <img src={Image}/>
+                            <div className="f-account-name">
+                                <h1>{accountName}</h1>
+                            </div>
+                        </div>
+
+                        <div className="f-account-status">
+
+                            <div className="f-member-info">
+                                <h3>{memberInfo} member</h3>
+                                <span>Expires in {expiryDay} {expiryRange}</span>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <Divider/>
+
+
+                    <div className="f-account-content">
+                        {/* json parser n loaders? */}
+                        <div className="f-account-content-header">
+                            Uploaded ({itemCount})
+                        </div>
+
+                        {/* sample json constructed content format */}
+                        <div className="f-account-tray">
+                            <div className="f-account-item">
+                                <div className="f-account-item-availability">_availability-icon_</div>
+                                <div className="f-account-item-name">_var-name_</div>
+                                <div className="f-account-item-size">_var-size_</div>
+                                <div className="f-account-item-share">_share_</div>
+                                <div className="f-account-item-delete">_trashcan_</div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
 
             <Footer />
             <div className="f-footer-spacer"></div>
