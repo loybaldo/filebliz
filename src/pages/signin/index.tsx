8336f096976bfbdb4db2baa1b04cf12da814fb32
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-19 09:06:46 +0800

    signin page updated

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,14 +1,20 @@
 import Footer from "../../components/custom/footer";
 import Navigation from "../../components/custom/navigation";
 import "./signin.scss";
+import GoogleLogo from "../../assets/google.svg";
+import FacebookLogo from "../../assets/facebook.svg";
 
 
 function SigninPage() {
     return (
         <>
             <Navigation/>
-            <div style={{marginTop: 60}}>
-                <h1>Signin Page!</h1>
+            <div style={{maxWidth: 1320, margin: "60px auto 0 auto", padding: "50px 0", textAlign: "center"}}>
+                <h1>Sign In Using:</h1>
+                <div className="f-signin-container">
+                    <button><img src={GoogleLogo} alt="Google Logo"/></button>
+                    <button><img src={FacebookLogo} alt="Facebook Logo"/></button>
+                </div>
             </div>
             <Footer/>
         </>
