fae3a395f7ea12ba4a7118b4cfede7df8a531b23
loybaldo <juanitobaldoaspler@gmail.com>
2023-03-20 22:24:09 +0800

    pricing card components updated

--- src/components/pages/pricing/offers/index.tsx
+++ src/components/pages/pricing/offers/index.tsx
@@ -3,11 +3,32 @@ import "./offers.scss";
 
 
 function Offers() {
+    const data = {
+        free: [
+            "Send up to 100Mb",
+            "Expires in 7 days",
+            "Photos, videos, and docs only",
+            "Share to anyone"
+        ],
+        pro: [
+            "Send up to 3Gb",
+            "Expires in 30 days",
+            "All file type can be uploaded",
+            "Share to anyone"
+        ],
+        premium: [
+            "Send up to 10Gb",
+            "Expires in 12 months",
+            "All file type can be uploaded",
+            "Share to anyone"
+        ]
+    }
+    
     return (
         <div className="f-offers">
-            <PricingCard type="free" price={0} shortDesc="For Getting Started" features={["Lorem ipsum", "Dolor sit amet"]} action={false}/>
-            <PricingCard type="pro" price={10} shortDesc="For Getting Started" features={["Lorem ipsum", "Dolor sit amet"]}/>
-            <PricingCard type="premium" price={18} shortDesc="For Getting Started" features={["Lorem ipsum", "Dolor sit amet"]}/>
+            <PricingCard type="free" price={0} features={data.free} action={false}/>
+            <PricingCard type="pro" price={10} features={data.pro}/>
+            <PricingCard type="premium" price={18} features={data.premium}/>
         </div>
     );
 }
--- src/components/pages/pricing/pricing-card/index.tsx
+++ src/components/pages/pricing/pricing-card/index.tsx
@@ -7,25 +7,21 @@ import "./pricing-card.scss";
 interface PricingCardInterface {
     type: "free" | "pro" | "premium";
     price: number;
-    shortDesc: string;
     features: string[];
     action?: boolean;
 }
 
-function PricingCard({type, price, shortDesc, features, action = true}: PricingCardInterface) {
+function PricingCard({type, price, features, action = true}: PricingCardInterface) {
     return(
         <div className="f-pricing-card">
-            <span> {type} </span>
-            <span> <b>{price}</b> Per Month</span>
-            <p> {shortDesc} </p>
-            <u>
-                <li><Icon color="#46BC4A" icon={Icons.check_circle}/> Lorem ipsum dolor sit amet cons ectetur.</li>
-                <li><Icon icon={Icons.check_circle}/> Lorem ipsum dolor sit amet con sectetur.</li>
-                <li><Icon icon={Icons.check_circle}/> Lorem ipsum dolor sit amet con sectetur.</li>
-                <li><Icon icon={Icons.check_circle}/> Lorem ipsum dolor sit amet con sectetur.</li>
-                <li><Icon icon={Icons.check_circle}/> Lorem ipsum dolor sit amet con sectetur.</li>
-            </u>
-            {(action) ? (<Button style={{margin:"25px"}} label="Purchase"/>) : null}
+            <div style={{padding: 25, display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <span> {type} </span>
+                <span> <b>${price}</b> Per Month</span>
+                <ul>
+                    {features.map((item, index) => (<li key={index}><Icon icon={Icons.check_circle}/> {item} </li>))}
+                </ul>
+                {(action) ? (<Button style={{margin:"25px"}} label="Purchase"/>) : null}
+            </div>
         </div>
     );
 }
