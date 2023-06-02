2db3f66378671ba73aebcf0960abdce33eebfa6d
loybaldo <juanitobaldoaspler@gmail.com>
2023-06-02 18:48:30 +0800

    test adunit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -17,6 +17,18 @@ function HomePage() {
                 <Advert />
                 <Services />
                 <Feedbacks />
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2476657885581568" crossOrigin="anonymous"></script>
+                <ins className="adsbygoogle"
+                    style={{display: "block"}}
+                    data-ad-format="fluid"
+                    data-ad-layout-key="-f1+5r+5a-db+57"
+                    data-ad-client="ca-pub-2476657885581568"
+                    data-ad-slot="6610420358">
+
+                </ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
                 <Campaign />
             </div>
             <Footer />
