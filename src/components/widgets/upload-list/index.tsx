097e765ffdd11fc35b253519f2eeb973f3d33683
loybaldo <juanitobaldoaspler@gmail.com>
2023-06-08 20:24:52 +0800

    drag & drop bug fixed

--- src/components/widgets/upload-list/index.tsx
+++ src/components/widgets/upload-list/index.tsx
@@ -9,9 +9,8 @@ import "./upload-list.scss";
 import { ref, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage";
 import Progress from "../../common/progress";
 
-
 function UploadList() {
-	const [uploadFiles, setUploadFiles] = useState<File | null>(null);
+	const [uploadFiles, setUploadFiles] = useState<FileList | null>(null);
 	const [progress, setProgress] = useState(0);
 	const { currentUser, memberships, files, getFiles } = useContext(AuthContext);
 
@@ -28,7 +27,6 @@ function UploadList() {
 		const q = query(fileRef, where("uploader", "==", currentUser?.uid));
 		const querySnapshot = await getDocs(q);
 		querySnapshot.forEach(async (doc) => {
-			console.log(doc.data().name);
 			// Delete file
 			const docRef = ref(storage, process.env.REACT_APP_UPLOAD_PATH + "/" + doc.data().genName);
 			await deleteObject(docRef);
@@ -37,13 +35,8 @@ function UploadList() {
 		});
 	}
 
-	const handleUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handleUpload = async (fileToUpload: File) => {
 		let expiration: number | null = null;
-		const filesToUpload = e.target.files;
-		if (!filesToUpload || filesToUpload.length === 0) {
-			return;
-		}
-		const fileToUpload = filesToUpload[0];
 		// Handle upload
 		if (memberships.length <= 0) {
 			expiration = new Date().getTime();
@@ -72,98 +65,89 @@ function UploadList() {
 		const uploadTask = uploadBytesResumable(fileRef, fileToUpload);
 
 		// Track the upload percentage
-		uploadTask.on(
-			"state_changed",
-			(snapshot) => {
-				const progress = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(1);
-				setProgress(+progress);
-				// ======================================
-				//    Set Upload indicator or a loader
-				// ======================================
-			},
-			(err) => {
+		uploadTask.on("state_changed", (snapshot) => {
+			const progress = ((snapshot.bytesTransferred / snapshot.totalBytes) * 100).toFixed(1);
+			setProgress(+progress);
+		}, (err) => {
+			console.error(err);
+		}, async () => {
+			try {
+				const downloadURL = await getDownloadURL(fileRef);
+				const fileInfo = {
+					uploader: currentUser ? currentUser.uid : null,
+					id: genID,
+					name: fileToUpload.name,
+					genName: genName,
+					type: fileToUpload.type,
+					size: fileToUpload.size,
+					expiration: expiration,
+					dateUploaded: new Date().toISOString(),
+					downloadURL: downloadURL,
+				};
+				await addDoc(collection(db, process.env.REACT_APP_UPLOAD_FIRESTORE_PATH!), fileInfo);
+				setProgress(0);
+			} catch (err) {
 				console.error(err);
-			},
-			async () => {
-				try {
-					const downloadURL = await getDownloadURL(fileRef);
-					const fileInfo = {
-						uploader: currentUser ? currentUser.uid : null,
-						id: genID,
-						name: fileToUpload.name,
-						genName: genName,
-						type: fileToUpload.type,
-						size: fileToUpload.size,
-						expiration: expiration,
-						dateUploaded: new Date().toISOString(),
-						downloadURL: downloadURL,
-					};
-					await addDoc(collection(db, process.env.REACT_APP_UPLOAD_FIRESTORE_PATH!), fileInfo);
-					setProgress(0);
-				} catch (err) {
-					console.error(err);
-				}
 			}
-		);
+		});
 	};
+
+	useEffect(() => {
+		if (uploadFiles && uploadFiles.length > 0) {
+			const fileToUpload = uploadFiles[0];
+			handleUpload(fileToUpload);
+		}
+	}, [uploadFiles]);
 
 	// use drag and drop
 	const onHover = 'hovering';
 	const onHoverOutside = 'notHovering';
-	const element = document.getElementById("dropdown-location");
 
-	// =============================================
-	//     		  Handle Drag and Drop
-	// =============================================
-	function handleDragOver(param: any, e: React.DragEvent<HTMLDivElement>): void {
+	const handleDragOver = (param: string, e: React.DragEvent<HTMLDivElement>): void => {
 		e.preventDefault();
 		e.stopPropagation();
-
 		updateLabelWithFileName('Drop Here!');
-
-		// perhaps simplify this?
-		if (param === 'hovering') {
-			updateLabelWithFileName('Ready!');
-			element?.classList.remove("dragging-over");
-			element?.classList.add("dragging-over-active");
-		} else if (param === 'notHovering') {
-			element?.classList.remove("dragging-over-active");
-			element?.classList.add("dragging-over");
+		const element = document.getElementById("dropdown-location");
+			if (element) {
+			if (param === 'hovering') {
+				updateLabelWithFileName('Ready!');
+				element.classList.remove("dragging-over");
+				element.classList.add("dragging-over-active");
+			} else if (param === 'notHovering') {
+				element.classList.remove("dragging-over-active");
+				element.classList.add("dragging-over");
+			}
 		}
-	}
+	};
 
-	function handleDragLeave(e: React.DragEvent<HTMLDivElement>): void {
+	const handleDragLeave = (e: React.DragEvent<HTMLDivElement>): void => {
 		e.preventDefault();
 		e.stopPropagation();
 
 		updateLabelWithFileName('Choose a File');
-		removeClass()
-	}
+		removeClass();
+	};
 
-	function handleDrop(e: React.DragEvent<HTMLDivElement>) {
+	const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault();
-		// setIsUploadDisabled(true);
-
 		const files = e.dataTransfer.files;
-		if (files != null && files.length > 0) {
-			// FIXME: trigger upload here
-			setUploadFiles(files[0]);
+			if (files != null && files.length > 0) {
+			setUploadFiles(files);
 			const fileName = files[0].name.trim();
 			updateLabelWithFileName(fileName);
 		}
+		removeClass();
+	};
 
-		removeClass()
-	}
-
-	function removeClass() {
+	const removeClass = () => {
+		const element = document.getElementById("dropdown-location");
 		if (element) {
 			element.classList.remove("dragging-over");
 			element.classList.remove("dragging-over-active");
 		}
-	}
+	};
 
-	// extracted filename handler
-	function updateLabelWithFileName(fileName: string) {
+	const updateLabelWithFileName = (fileName: string) => {
 		const maxFileNameLength = 13;
 
 		if (fileName.length > maxFileNameLength) {
@@ -180,41 +164,54 @@ function UploadList() {
 				label.innerHTML = fileName;
 			}
 		}
-	}
+	};
 
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const files = e.target.files;
+		if (files) {
+			setUploadFiles(files);
+		}
+	};
 
 	return (
 		<>
-			<Progress show={(progress > 0) ? true : false} percent={progress}/>
-
+			<Progress show={progress > 0} percent={progress} />
 			<div className="f-upload-box" onDragOver={(e) => handleDragOver(onHoverOutside, e)} onDragLeave={handleDragLeave}>
-				<div className="drop-area"
+				<div
+					className="drop-area"
 					id="dropdown-location"
 					onDragOver={(e) => handleDragOver(onHover, e)}
 					onDragLeave={handleDragLeave}
-					onDrop={handleDrop}
-				>
-
+					onDrop={handleDrop}>
 					<label htmlFor="file-upload" className="account-file-upload" style={{ display: "flex", justifyContent: "center" }}>
 						<i className="fa-regular fa-cloud-arrow-up" style={{ marginRight: 15, fontSize: "x-large" }}></i>
 						<span id="file-label">Drop Files Here</span>
-						<input id="file-upload" type="file" onChange={handleUpload} />
+						<input id="file-upload" type="file" onChange={handleChange} />
 					</label>
-
 				</div>
 			</div>
-
 
 			<div className="f-upload-list">
 				<div className="f-del-all-wrapper">
 					<span className="f-label">Uploaded ({files.length})</span>
-					<Button onclick={handleDeleteAll} classItem={"danger"}> Delete All </Button>
+					<Button onclick={handleDeleteAll} classItem={"danger"}>
+						Delete All
+					</Button>
 				</div>
-				{(files.length === 0) ?
-					(<div className="f-upload-list-no-data">
-						<p>No Uploaded Files</p>
-					</div>) : null}
-				{files.map((file) => (<ListView key={Math.random()} id={file.id} docId={file.docId} fileName={file.name} genFileName={file.genName} size={file.size} fileExt={file.type.split("/")[1]} url={file.downloadURL} date={file.dateUploaded.split("T")[0]} />))}
+				{files.length === 0 ? (<div className="f-upload-list-no-data"><p>No Uploaded Files</p></div>) : null}
+				{files.map((file) => (
+				<ListView
+					key={Math.random()}
+					id={file.id}
+					docId={file.docId}
+					fileName={file.name}
+					genFileName={file.genName}
+					size={file.size}
+					fileExt={file.type.split("/")[1]}
+					url={file.downloadURL}
+					date={file.dateUploaded.split("T")[0]}
+				/>
+				))}
 			</div>
 		</>
 	);
